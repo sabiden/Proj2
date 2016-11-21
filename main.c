3e64f32680ec30e3b2940bfa91122f1a60b96af4
Jack Schluger <jschluger@stuy.edu>
2016-11-21 18:29:04 -0500

    did cd

--- main.c
+++ main.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include "main.h"
 
-void runcd(){
+void runcd( char target[] ){
+  int err;
+  err = chdir( target );
+  if (err)
+    printf("-shell: cd: %s: %s\n", target, strerror( errno ));
 }
 
 int main(){
+
   while(1){
     char dest[256];
     char cwd[100];
-    getcwd(&cwd,sizeof(cwd));
+    getcwd( cwd, sizeof(cwd) );
     printf("%s$ ",cwd );
     fgets(dest, 256, stdin);
     
@@ -18,13 +25,13 @@ int main(){
     
     char *s = dest;
     char * command[100];
+
     int i;
-    
     for(i = 0; s; command[i] = strsep( &s, " "), i++);
     command[i]=0;
-
+    
     if(!(strcmp("cd",command[0]))){
-      runcd();
+      runcd( command[1] );
     }
     else{
       int f;
@@ -33,7 +40,8 @@ int main(){
 	execvp(command[0], command);
       }
       else{
-	wait();
+	int stats;
+	wait( &stats );
       }
     }
   }
