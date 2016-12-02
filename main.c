d970d10f159de182d9212df6cf0fb5bda66877d5
Jack Schluger <jschluger@stuy.edu>
2016-12-01 20:19:00 -0500

    woah. thats a lot of stuff

--- main.c
+++ main.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include "main.h"
-#include <fcntl.h>
-#include <readline/readline.h>
-#include <readline/history.h>
 
 char *prompt() {
   char fullcwd[100];
@@ -31,12 +23,12 @@ char *prompt() {
   return retString;
 }
 
-void runcd( char target[] ){
+void runcd( char *target ){
   int err;
   if ( target )
     err = chdir( target );
   else
-    err = chdir("~");
+    err = chdir( 0 );
   if (err)
     printf("-shell: cd: %s: %s\n", target, strerror( errno ));
 }
@@ -50,8 +42,29 @@ int finder(char *command[], char *arg ){
   }
   return 0;
 }
-	 
-int execute( char *input ) {
+
+// int executeLine( char *in )
+// in: a line read in using readline(), stored as a char*
+// returns 0 on exit, else 1
+int executeLine( char *in ) {
+  int status = 1;
+  char *s = in;
+  char * line[15];
+  
+  int i;
+  for(i = 0; (line[i] = strsep( &s, ";")); i++);    
+    
+  for(i = 0; line[i]; i++) {
+    status = status && executeCommand( line[i] );
+  }
+
+  return status;
+}
+
+// int executeCommand( char *input )
+// input: a single command (no ;'s), stored as a char*
+// returns 0 on exit, else 1
+int executeCommand( char *input ) {
   char *s = input;
   char * command[100];
 
@@ -60,119 +73,118 @@ int execute( char *input ) {
   for(i = 0; (command[i] = strsep( &s, " ")); i++)
     if ( !strlen(command[i]) ) //to get rid of multiple spaces
       i--;
-  
-  /* for (i = 0; command[i]; i++) */
-  /*   printf("command[%d]: %s, size: %lu\n", i, command[i], strlen(command[i])); */
+
+  if (!i) return 1;//if the user only entered spaces
   
   if ( !strcmp("exit",command[0]))
     return 0;
   else if( !(strcmp("cd",command[0])) )
     runcd( command[1] );
   
-  else{
+  else {
     int f;
     f = fork();
-      
-    if ( !f ){
-      
-      int loc;
 
-      if( (loc = finder(command,">")) ) {
-	char *file = command[ loc + 1];
-	command[loc] = 0;
-	int fd= open(file, O_CREAT | O_RDWR, 0644);
-	dup2(fd,STDOUT_FILENO);
-	execvp(command[0], command);
-      }
-      
-      else if( (loc = finder(command,">>")) ) {
-	char *file = command[ loc + 1];
-	command[loc] = 0;
-	int fd= open(file, O_APPEND | O_CREAT | O_RDWR, 0644);
-	dup2(fd,STDOUT_FILENO);
-	execvp(command[0], command);
-      }
-
-      else if( (loc = finder(command,"&>")) ) {
-	char *file = command[ loc + 1];
-	command[loc] = 0;
-	int fd= open(file, O_CREAT | O_RDWR, 0644);
-	dup2(fd,STDOUT_FILENO);
-	dup2(fd,STDERR_FILENO);
-	execvp(command[0], command);
-      }
-      else if( (loc = finder(command,"2>")) ) {
-	char *file = command[ loc + 1];
-	command[loc] = 0;
-	int fd= open(file, O_CREAT | O_RDWR, 0644);
-	dup2(fd,STDERR_FILENO);
-	execvp(command[0], command);
-      }
-      
-      else if( (loc = finder(command,"<")) ) {
-	char *file = command[ loc + 1];
-	command[loc] = 0;
-	int fd = open(file, O_CREAT | O_RDWR, 0644);
-	dup2(fd,STDIN_FILENO);
-	execvp(command[0], command);	
-      }
-
-      else if( (loc = finder(command,"|")) ) {
-	char **command2 = &command[ loc + 1 ];
-	command[loc] = 0;
-	int fds[2];
-	pipe( fds );
-	
-	int f2 = fork();
-	if (!f2) {
-	  close( fds[0] );
-	  dup2( fds[1], STDOUT_FILENO );
-	  execvp(command[0], command);
-	}
-	else {
-	  int stats;
-	  wait( &stats );
-	  close( fds[1] );
-	  dup2( fds[0], STDIN_FILENO );
-	  execvp(command2[0], command2);
-	}
-      }
-      
-      else{
-	execvp(command[0], command);	
-      }
-    }
-    else{
-      int stats;
-      wait( &stats );
-    }
+    executeFork( command, f );
   }
   return 1;
 }
 
+// void executeFork( char *command[] )
+// command: an array of strings created by delimiting input to executeCommand by " "
+// f: return value from fork() call
+// no returns
+void executeFork(char *command[], int f) {
+  if ( !f ){
+      
+    int loc;
 
-int main(){
-  int status = 1;
-  
-  while( status ){
-    char *pmt = prompt();
+    if( (loc = finder(command,"|")) ) {
+      char **command2 = &command[ loc + 1 ];
+      command[loc] = 0;
+      int fds[2];
+      pipe( fds );
+	
+      int f2;
+      f2 = fork();
+      if (!f2) {
+	close( fds[0] );
+	dup2( fds[1], STDOUT_FILENO );
+	executeFork( command, f );
+      }
+      else {
+	int stats;
+	wait( &stats );
+	close( fds[1] );
+	dup2( fds[0], STDIN_FILENO );
+	executeFork( command2, f );
+      }
+    }
+    else if( (loc = finder(command,">")) ) {
+      char *file = command[ loc + 1];
+      command[loc] = 0;
+      int fd= open(file, O_CREAT | O_RDWR, 0644);
+      dup2(fd,STDOUT_FILENO);
+      executeFork( command, f );
+    }
+      
+    else if( (loc = finder(command,">>")) ) {
+      char *file = command[ loc + 1];
+      command[loc] = 0;
+      int fd= open(file, O_APPEND | O_CREAT | O_RDWR, 0644);
+      dup2(fd,STDOUT_FILENO);
+      executeFork( command, f );
+    }
+
+    else if( (loc = finder(command,"&>")) ) {
+      char *file = command[ loc + 1];
+      command[loc] = 0;
+      int fd= open(file, O_CREAT | O_RDWR, 0644);
+      dup2(fd,STDOUT_FILENO);
+      dup2(fd,STDERR_FILENO);
+      executeFork( command, f );
+    }
+    else if( (loc = finder(command,"2>")) ) {
+      char *file = command[ loc + 1];
+      command[loc] = 0;
+      int fd= open(file, O_CREAT | O_RDWR, 0644);
+      dup2(fd,STDERR_FILENO);
+      executeFork( command, f );
+    }
+      
+    else if( (loc = finder(command,"<")) ) {
+      char *file = command[ loc + 1];
+      command[loc] = 0;
+      int fd = open(file, O_CREAT | O_RDWR, 0644);
+      dup2(fd,STDIN_FILENO);
+      executeFork( command, f );	
+    }
     
-    //get input
-    //fgets(in, sizeof(in), stdin);
-    //get rid of new line
-    //*strchr(in, '\n') = 0;
-    char *in = readline( pmt );
-    
-    char *s = in;
-    char * line[15];
-
-    int i;
-    for(i = 0; s; line[i] = strsep( &s, ";"), i++);    
-    line[i]=0;
-
-    for(i = 0; line[i]; i++) {
-      status = status && execute( line[i] );
+    else{
+      execvp(command[0], command);	
     }
   }
+  else{
+    int stats;
+    wait( &stats );
+  }
+}
+// int main
+// returns 0
+int main(){
+  int status = 1;
+    
+  while( status ){
+    //get input
+    char *pmt = prompt();
+    char *in = readline( pmt );
+
+    if ( strlen(in) ) //dont add empty commands to the history
+      add_history( in );
+    
+    status = executeLine( in );
+    
+  }
+  printf(" goodbye\n");
   return 0;
 }
---
+++ main.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+char *prompt();
+
+void runcd( char * );
+
+int finder(char *command[], char * );
+
+int executeLine( char * );
+
+int executeCommand( char *);
+
+void executeFork(char *command[], int );
